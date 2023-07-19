1a657c3179c2ba3ca9c889cbd49f6cabb2fa120b
Matt Faucher <mfauch@amazon.com>
2023-07-19 18:03:57 +0000

    update floaterm settings/keymaps

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,13 +1,14 @@
--- Create a new floatterm & Kill the window
-vim.keymap.set("n", "<leader>tf", vim.cmd.FloatermNew)
-vim.keymap.set("t", "<C-l>", vim.cmd.FloatermKill)
-vim.keymap.set("n", "<leader>ftl", vim.cmd.FloatermLast)
+-- Toggle existing float term
+vim.keymap.set("n", "<C-h>", vim.cmd.FloatermToggle)
+vim.keymap.set("t", "<C-h>", vim.cmd.FloatermToggle)
+-- Kill floaterm
+vim.keymap.set("t", "<C-k>", vim.cmd.FloatermKill)
 -- Open Python shell
 vim.keymap.set("n", "<leader>tfp", function()
     vim.cmd.FloatermNew "python3"
 end)
 
--- Set float term window
+-- Set float term window dimensions
 vim.g.floaterm_width = 0.8
 vim.g.floaterm_height = 0.6
 vim.g.floaterm_position = "center"
