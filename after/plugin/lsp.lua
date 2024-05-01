3dd50cd79aa9f76702081caa3b3db3c078471d29
Matt Faucher <mfauch@amazon.com>
2024-05-01 15:34:35 +0000

    Setup bemol for LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,6 +32,25 @@ end
 
 SetBrazilJDKHome()
 
+function Bemol()
+	local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory'})[1]
+	local ws_folders_lsp = {}
+	if bemol_dir then
+		local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+		if file then
+
+			for line in file:lines() do
+				table.insert(ws_folders_lsp, line)
+			end
+			file:close()
+		end
+	end
+
+	for _, line in ipairs(ws_folders_lsp) do
+		vim.lsp.buf.add_workspace_folder(line)
+	end
+end
+
 lsp.ensure_installed({
   'tsserver',
   'eslint',
@@ -117,6 +136,7 @@ lsp.on_attach(function(client, bufnr)
     }
     vim.cmd(':w')
   end, opts)
+  Bemol()
 end)
 
 lsp.setup()
