11d545d6c2230b6b1035e1245054e0eedb96f242
Matt Faucher <mfauch@amazon.com>
2024-10-24 13:43:26 +0000

    Enable tmux passthrough

--- after/plugin/osc52.lua
+++ after/plugin/osc52.lua
@@ -2,7 +2,7 @@ require('osc52').setup {
     max_length = 0,
     silent = false,
     trim = false,
-    tmux_passthrough = false,
+    tmux_passthrough = true,
 }
 
 vim.keymap.set('n', '<leader>yf', require('osc52').copy_operator, { expr = true })
