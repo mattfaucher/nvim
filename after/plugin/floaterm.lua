6404d34eddb3e10394124f95b71ae6e7cd76aa08
Matt Faucher <mfauch@amazon.com>
2025-08-05 19:05:24 +0000

    Add floaterm shortcuts

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -3,6 +3,12 @@ vim.keymap.set("n", "<C-h>", vim.cmd.FloatermToggle)
 vim.keymap.set("t", "<C-h>", vim.cmd.FloatermToggle)
 -- Kill floaterm
 vim.keymap.set("t", "<C-k>", vim.cmd.FloatermKill)
+-- Next floaterm
+vim.keymap.set("t", "<C-n>", vim.cmd.FloatermNext)
+-- Prev floaterm
+vim.keymap.set("t", "<C-p>", vim.cmd.FloatermPrev)
+-- New floaterm
+vim.keymap.set("t", "<C-m>", vim.cmd.FloatermNew)
 -- Open Python shell
 vim.keymap.set("n", "<leader>tfp", function()
     vim.cmd.FloatermNew "python3"
